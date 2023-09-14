524092d072f34d42f7e1242a5c84146a97f970ba
Arduguy <nansy16@>
2023-09-14 12:32:16 +0600

    1

--- main.py
+++ main.py
@@ -34,12 +34,6 @@ def who(message):
 def start(message):
     user_id = message.from_user.id        # Визначаєм id користувача
     username = message.from_user.first_name # Визначаєм ім'я користувача
-    print("Hello")
-    db_object.execute("INSERT INTO users(id, access_level, passed_tests) VALUES (%s, %s, %s)", (message.from_user.id, 0, "#"))
-    print("Hello")
-    db_connection.commit()
-    print("Hello")
-    bot.send_message(message.chat.id, message)
     bot.send_message(message.chat.id, who(message))
     # #db_object.execute(f"SELECT lang FROM users WHERE id = {message.from_user.id}") 
     # result = db_object.fetchone()
