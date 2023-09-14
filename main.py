9887d99c5a45810df6da584189490fb4296882a9
Arduguy <nansy16@>
2023-09-14 17:56:59 +0600

    1

--- main.py
+++ main.py
@@ -58,6 +58,8 @@ def user_step_1(message, start = False):
     markup.add(new_topic, go_over_the_topic)                # Тоже для кнопочок
     if passed_tests == [-1] and start:
         bot.send_message(message.chat.id, f"Привіт, {message.from_user.username}, бажаємо тобі вдало пройти весь курс, удачі", reply_markup=markup)
+    if passed_tests == [-1]:
+        bot.send_message(message.chat.id, f'Вибери тему, або натисни "Нова тема", щоб почати', reply_markup=markup)
     else:
         bot.send_message(message.chat.id, f"Ти пройшов такі теми, як /{', /'.join(passed_tests)}", reply_markup=markup)
 
