8d37d5292efa9f2e4e3098f0be9bd2c6a14426f7
Arduguy <nansy16@>
2023-09-14 11:12:27 +0600

    1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 import psycopg2
+import logging
 from lang_pack import *
 from config import *
 from telebot import types
@@ -8,6 +9,8 @@ from flask import Flask, request, render_template
 
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 
 db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
@@ -31,14 +34,15 @@ db_object = db_connection.cursor()
 def start(message):
     user_id = message.from_user.id        # Визначаєм id користувача
     username = message.from_user.first_name # Визначаєм ім'я користувача
-    bot.send_message(message.chat.id, message)
+    
     db_object.execute(f"SELECT access level FROM users WHERE id = {message.from_user.id}") # Беремо з бази мову користувача та чи він забанений
     result = db_object.fetchone()
-    if result == None:
-        db_object.execute("INSERT INTO users(id, access level, passed tests) VALUES (%s, %s, %s)", (message.from_user.id, 0, "#"))
-        bot.send_message(message.chat.id, result)
-    else:
-        bot.send_message(message.chat.id, result) # Якщо користувач вже відомий, повертається його рівень доступу
+    bot.send_message(message.chat.id, str(result))
+    # if result == None:
+    #     db_object.execute("INSERT INTO users(id, access level, passed tests) VALUES (%s, %s, %s)", (message.from_user.id, 0, "#"))
+    #     bot.send_message(message.chat.id, result)
+    # else:
+    #     bot.send_message(message.chat.id, result) # Якщо користувач вже відомий, повертається його рівень доступу
     # #db_object.execute(f"SELECT lang FROM users WHERE id = {message.from_user.id}") 
     # result = db_object.fetchone()
     # is_banned_variable = is_banned(message)
